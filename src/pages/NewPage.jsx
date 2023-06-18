4473a1c46ffea5a78dae71ab5391bc9c83a660ce
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-06-18 18:51:55 +0300

    added new page

---
+++ src/pages/NewPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NewComp from '../components/NewComp';
+
+function NewPage() {
+  return (
+    <>
+        <h1>Inside NewPage</h1>
+        <NewComp/>
+    </>
+  )
+}
+
+export default NewPage;
