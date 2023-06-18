40ae3714be032c2b981936ed10fa80fb07c08156
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-06-18 18:20:17 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<h1 className="text-3xl font-bold underline">
+				Components Or Pages goes here!
+			</h1>
+		</>
+	);
+}
+
+export default App;
