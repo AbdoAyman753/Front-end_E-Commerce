5d8d5f5ee75524d033918cd537e80e6718dc49a5
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-06-18 18:52:21 +0300

    tested component and page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
 import "./App.css";
+import NewPage from './pages/NewPage';
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 			<h1 className="text-3xl font-bold underline">
 				Components Or Pages goes here!
 			</h1>
+			<NewPage/>
 		</>
 	);
 }
