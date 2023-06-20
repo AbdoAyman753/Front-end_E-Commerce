b49778d4d556d87ce88e764698532f22b0b51204
AbdallahM22 <abdallahmbedo22@gmail.com>
2023-06-20 21:43:13 +0300

    configured container and used it

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-cyan-950 text-white py-4 fixed left-0 right-0 bottom-0 w-100">
-      Footer
+      <div className="container">footer</div>
     </div>
   );
 };
