abac2422a72b060208a842e5bb0d858915921ff3
AbdallahM22 <abdallahmbedo22@gmail.com>
2023-06-24 12:28:42 +0300

    modified cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -21,38 +21,12 @@ const ICON = (
   </svg>
 );
 
-// const CART = [
-//   {
-//     _id: 1,
-//     product_name: "1st game",
-//     description:
-//       " minus animi vel alias corporis quibusdam tempore possimus quia voluptatibus!",
-//     price: 25,
-//     quantity: 1,
-//   },
-//   {
-//     _id: 2,
-//     product_name: "2nd game",
-//     description: "us quia accusantium maiores saepe blanditiis enim quis ",
-//     price: 18,
-//     categoryId: 1,
-//     quantity: 1,
-//   },
-//   {
-//     _id: 3,
-//     product_name: "3rd game",
-//     description: "Lorem ipsum dolor sit amet consectetur",
-//     price: 37,
-//     categoryId: 3,
-//     quantity: 1,
-//   },
-// ];
+
 const Cart = () => {
   const { cart } = useSelector((state) => state.cart);
   const total = useSelector(totalPrice);
   const dispatch = useDispatch();
 
-  //   if (cart.length < 1) return
   return (
     <div className="container py-14">
       <Link
@@ -101,4 +75,3 @@ const Cart = () => {
 
 export default Cart;
 
-// export default Cart
