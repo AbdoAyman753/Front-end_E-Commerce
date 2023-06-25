497414784780b3a1ecf3054bff5f4fa1b024e695
AbdallahM22 <abdallahmbedo22@gmail.com>
2023-06-25 23:20:23 +0300

    Revert "finished information edit"
    This reverts commit 9474482fd3fc09289b54e39007a14eda2c098850.

--- src/pages/StorePage.jsx
+++ src/pages/StorePage.jsx
@@ -85,6 +85,7 @@ const StorePage = () => {
     const fetchGames = async () => {
       const { data } = await axios.get(
         "http://localhost:3000/products?_delay=0"
+
       );
 
       setGames(data);
@@ -218,55 +219,56 @@ const StorePage = () => {
                     </svg>
                   </span>
 
-                  {/* heart icon */}
-                  <span
-                    onClick={() => dispatch(addToWishlist(game))}
-                    className=" rounded-full   text-sm font-semibold text-white  hover:scale-110 "
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6  hover:stroke-sky-300  cursor-pointer "
-                    >
-                      <title>Add To WishList</title>
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                      />
-                    </svg>
-                  </span>
 
-                  {/* add to cart icon*/}
-                  <span
-                    onClick={() => {
-                      dispatch(addToCart(game));
-                    }}
-                    className="rounded-full   text-sm font-semibold text-white hover:scale-110    "
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6  hover:stroke-sky-300  "
+                    {/* heart icon */}
+                    <span
+                      onClick={() => dispatch(addToWishlist(game))}
+                      className=" rounded-full   text-sm font-semibold text-white  hover:scale-110 "
                     >
-                      <title>Add To Cart</title>
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                      />
-                    </svg>
-                  </span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6  hover:stroke-sky-300  cursor-pointer "
+                      >
+                        <title>Add To WishList</title>
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                        />
+                      </svg>
+                    </span>
+
+                    {/* add to cart icon*/}
+                    <span
+                      onClick={() => {
+                        dispatch(addToCart(game));
+                      }}
+                      className="rounded-full   text-sm font-semibold text-white hover:scale-110    "
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6  hover:stroke-sky-300  "
+                      >
+                        <title>Add To Cart</title>
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                      </svg>
+                    </span>
+                  </div>
                 </div>
               </div>
             </div>
-            // </div>
             //   ))
             //   filteredGames.length > 0 ? (  ) : (
             //   <div className=" text-red-500 font-bold m-auto w-full md:ms-[25vw] col-span-2">
