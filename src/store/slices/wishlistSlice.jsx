26b305e709d6d22c6431cc7a19b89e728d7154f2
Abdallah Mohamed <83477790+AbdallahM22@users.noreply.github.com>
2023-06-26 16:36:20 +0300

    Update wishlistSlice

--- src/store/slices/wishlistSlice.jsx
+++ src/store/slices/wishlistSlice.jsx
@@ -1,11 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-<<<<<<< Updated upstream
-  wishlist: [],
-=======
   wishlist: JSON.parse(localStorage.getItem("wishlist")) || [],
->>>>>>> Stashed changes
 };
 
 const wishlistSlice = createSlice({
@@ -15,32 +11,21 @@ const wishlistSlice = createSlice({
     addToWishlist: (state, action) => {
       // check if is in wishlist
       const item = state.wishlist.find((el) => el._id === action.payload._id);
-<<<<<<< Updated upstream
-      console.log(item);
-      !item ? state.wishlist.push(action.payload) : null;
-=======
+
       !item ? state.wishlist.push(action.payload) : null;
       localStorage.setItem("wishlist", JSON.stringify(state.wishlist));
->>>>>>> Stashed changes
     },
     removeFromWishlist: (state, action) => {
       state.wishlist = state.wishlist.filter(
         (item) => item._id !== action.payload
       );
-<<<<<<< Updated upstream
-    },
 
-    clearWishlist: (state) => {
-      console.log("wish");
-      state.wishlist = [];
-=======
       localStorage.setItem("wishlist", JSON.stringify(state.wishlist));
     },
 
     clearWishlist: (state) => {
       state.wishlist = [];
       localStorage.removeItem("wishlist");
->>>>>>> Stashed changes
     },
   },
 });
