bf4f5b8e7c3c2df2390f18bd5236c12ff2ac80a8
AbdallahM22 <abdallahmbedo22@gmail.com>
2023-07-05 10:19:29 +0300

    added home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Carousel from "../components/Carousel";
+import Slider from "../components/Slider";
+
+const Home = () => {
+  return (
+    <>
+      <Carousel />
+      <Slider />
+    </>
+  );
+};
+
+export default Home;
