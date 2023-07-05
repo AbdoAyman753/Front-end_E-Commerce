3141f21b36d83ef0f8d358fe77a110b7768e893b
MariamSherif26 <127598939+MariamSherif26@users.noreply.github.com>
2023-07-05 09:29:21 +0300

    added carsosel, slider and about

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
+import React ,{ useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import NewPage from './pages/NewPage';
+// import NewPage from './pages/NewPage';
+// import Header from './components/Header'
+import Carousel  from "./components/Carousel";
+
+import Slider from "./components/Slider";
+// import About from "./components/About";
+
 
 function App() {
+
 	return (
 		<>
-			<h1 className="text-3xl font-bold underline">
-				Components Or Pages goes here!
-			</h1>
-			<NewPage/>
+			{/* <Header/> */}
+			<Carousel />	
+			<Slider/> 
+		{/* <About/> */}
+			
 		</>
+		
+		
 	);
 }
 
