1940a29910fd4a97bd586c57b71676eaa8095e5e
AbdallahM22 <abdallahmbedo22@gmail.com>
2023-07-08 00:09:16 +0300

    changed response structure

--- src/components/UserGames.jsx
+++ src/components/UserGames.jsx
@@ -3,13 +3,18 @@ import { useOutletContext } from "react-router";
 
 const UserGames = () => {
   const { library } = useOutletContext();
-  if (library.products.length < 1) return <h2>empty library</h2>;
+  const products = library[0].products
+  if (products.length < 1) return <h2>empty library</h2>;
   return (
     <div className="w-3/4 sm:w-full m-auto grid sm:grid-cols-2 md:grid-cols-3 gap-3 sm:ml-2 py-4">
-      {library.products.map((product) => (
+      {products.map((product, i) => (
         <div key={product._id} className="border p-1 rounded">
           <div className="">
-            <img className="w-full h-80" src={product.imgs_links} alt="game" />
+            <img
+              className="w-full h-80"
+              src={product.imgs_links[i]}
+              alt="game"
+            />
           </div>
           <div className="p-3">
             <h3>{product.product_name}</h3>
