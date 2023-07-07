681bc8a702e69a6bed6fc97c94fc438a88544a7d
AbdallahM22 <abdallahmbedo22@gmail.com>
2023-07-08 00:10:48 +0300

    handled image uploading

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -9,18 +9,34 @@ import "filepond/dist/filepond.min.css";
 import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+import useAuthenticate from "../utils/useAuthenticate";
 
 // Register the plugins
 registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
 
 const Picture = () => {
   const [files, setFiles] = useState([]);
+  const { token, id } = useAuthenticate();
 
-  const handleChangePicture = () => {
-    const file = files[0];
+  const handleChangePicture = async () => {
+    console.log(files);
+    const file = files[0].file;
+    console.log(file);
     const formData = new FormData();
-    formData.append("file", file);
-    axios.post("http://localhost:3000/images", formData);
+
+    formData.append("image", file);
+    console.log(formData.get("image"));
+    const response = await axios.patch(
+      `http://localhost:8000/users/${id}/profile_pic`,
+      formData,
+      {
+        headers: {
+          Authorization: token,
+          "content-type": "multipart/form-data",
+        },
+      }
+    );
+    console.log(response);
   };
 
   return (
@@ -43,7 +59,7 @@ const Picture = () => {
         />
       </div>
       <button
-        // onClick={handleChangePicture}
+        onClick={handleChangePicture}
         className="border rounded-full  bg-cyan-800 text-sm text-white w-auto h-10 p-2"
       >
         Change
