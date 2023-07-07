b9f42cc6744071b357fbd32e957909dc79536701
AbdallahM22 <abdallahmbedo22@gmail.com>
2023-07-08 00:12:07 +0300

    finished payment

--- src/utils/stripe.js
+++ src/utils/stripe.js
@@ -4,18 +4,23 @@ const stripe = Stripe(
   "pk_test_51NPT70BJTe270Y27mGLAncj6wPmG167SnKqen3uaK028mgMk0hsXdNKks3kKl7XOb4HoRZPoi4iJsCv65i6Hf4ZA00dyO7gHYw"
 );
 
-export const order = async (cart) => {
+export const order = async (cart, totalPrice, token) => {
   let session;
   try {
     session = await axios.post(
       "http://127.0.0.1:8000/create-checkout-session",
-      cart
+      { cart, totalPrice },
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
     );
-  } catch (error) {
-    console.log("something went wrong");
-  }
 
-  await stripe.redirectToCheckout({
-    sessionId: session.data.session.id,
-  });
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
