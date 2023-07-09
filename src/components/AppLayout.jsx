109cbe292addc45dd9c078b4fe5feb495d84365f
AbdallahM22 <abdallahmbedo22@gmail.com>
2023-07-09 19:21:54 +0300

    fixed bugs in appLayout

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,96 +1,58 @@
 /* eslint-disable react/prop-types */
 import React, { useEffect, useState } from "react";
-import Header from "./Header";
-import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
+
+import Header from "./Header";
+import Footer from "./Footer";
+import Loader from "./Loader";
 import { setCart } from "../store/slices/cartSlice";
 import { setWishlist } from "../store/slices/wishlistSlice";
 import { login } from "../store/slices/authSlice";
 
-// let firstRender = true;
-
 const AppLayout = () => {
-  // const [firstRender, setFirstRender] = useState(true);
   const dispatch = useDispatch();
   const { cart } = useSelector((state) => state.cart);
   const { wishlist } = useSelector((state) => state.wishlist);
-  const token = localStorage.getItem("token");
 
+  const [isLoading, setIsLoading] = useState(false);
+
+  const token = localStorage.getItem("token");
   const userId = localStorage.getItem("userId");
-  // console.log(wishlist);
+
   useEffect(() => {
     // if user => get user with its cart and wishlsit
-    // let userCart;
     const getUser = async () => {
-      const response = await axios({
-        method: "get",
-        url: `http://localhost:8000/users/${userId}`,
-        headers: {
-          Authorization: token,
-        },
-      });
-      console.log(response);
+      setIsLoading(true);
+      const response = await axios.get(
+        `http://localhost:8000/users/${userId}`,
+        {
+          headers: {
+            Authorization: token,
+          },
+        }
+      );
+      // console.log(response);
+      setIsLoading(false);
       if (response.status === 200) {
         const user = response.data;
         const userInfo = { ...user, cart: undefined, wishlist: undefined };
-        // const userInfo = { ...user };
         dispatch(setCart(user.cart[0].products));
         dispatch(setWishlist(user.wishlist[0].products));
         dispatch(login({ token, userInfo }));
       }
     };
     if (token && userId) {
-      getUser();
+      try {
+        getUser();
+      } catch (error) {
+        // console.log(error.message);
+      }
     }
-    //  if user save user cart and wishlist
-    // return () => {
-    //   const cart = JSON.parse(localStorage.getItem("cart"));
-    //   const wishlist = JSON.parse(localStorage.getItem("wishlist"));
-
-    //   const cartIds = cart
-    //     ? cart.map((product) => {
-    //         return product._id;
-    //       })
-    //     : [];
-
-    //   const wishlistIds = wishlist
-    //     ? wishlist.map((product) => {
-    //         return product._id;
-    //       })
-    //     : [];
-
-    //   const sendCartToServer = async () => {
-    //     const cartResponse = await axios.patch(
-    //       "http://localhost:8000/carts/updateCart",
-    //       { products: cartIds },
-    //       {
-    //         headers: {
-    //           Authorization: token,
-    //         },
-    //       }
-    //     );
-    //     console.log(cartResponse);
-    //     const wishlistResponse = await axios.patch(
-    //       "http://localhost:8000/wishlists/",
-    //       { products: wishlistIds },
-    //       {
-    //         headers: {
-    //           Authorization: token,
-    //         },
-    //       }
-    //     );
-    //     // console.log(response);
-    //   };
-    //   if (token) {
-    //     sendCartToServer();
-    //   }
-    // };
-  }, [dispatch, token, userId]);
+  }, [dispatch]);
 
   useEffect(() => {
-    // if (firstRender)
     const cartIds = cart
       ? cart.map((product) => {
           return product._id;
@@ -107,13 +69,15 @@ const AppLayout = () => {
           },
         }
       );
-      console.log(cartResponse);
+      // console.log(cartResponse);
     };
-    // console.log(firstRender);
     if (token) {
-      sendCartToServer();
+      try {
+        sendCartToServer();
+      } catch (error) {
+        // console.log(error);
+      }
     }
-    // firstRender = false;
   }, [cart]);
 
   useEffect(() => {
@@ -133,19 +97,23 @@ const AppLayout = () => {
           },
         }
       );
-      console.log(wishlistResponse);
+      // console.log(wishlistResponse);
     };
-    // console.log(firstRender);
     if (token) {
-      sendWishlistToServer();
+      try {
+        sendWishlistToServer();
+      } catch (error) {
+        // console.log(error);
+      }
     }
-    // firstRender = false;
   }, [wishlist]);
+
   return (
     <div className="flex flex-col">
       <Header />
       <main className="min-h-[80vh] flex flex-col">
-        <Outlet />
+        {isLoading && <Loader />}
+        {!isLoading && <Outlet />}
       </main>
       <Footer />
     </div>
