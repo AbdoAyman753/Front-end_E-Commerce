9f222e24405aec8ea6b53f50f75ed338c284b9b5
HebaAli48 <131808003+HebaAli48@users.noreply.github.com>
2023-07-11 23:21:17 +0300

    used button component

--- src/components/filter/SimpleGameFilter.jsx
+++ src/components/filter/SimpleGameFilter.jsx
@@ -1,24 +1,14 @@
 import React from "react";
-
-// json-server --watch db.json
-
+import Button from "../ui/Button";
 const SimpleGameFilter = ({
   handleGategoryFilter,
-
   selectedCategory,
-
   categories,
-
   setCurrentPage,
-
   setselectedCategory,
-
   handlePriceFilter,
-
   selectedPrice,
-
   filteredGames,
-
   games,
 }) => {
   return (
@@ -26,25 +16,25 @@ const SimpleGameFilter = ({
       {games.length > 0 && (
         <div className="mx-5 xs:mx-auto ">
           {/* category filter  */}
-
           <div className="  xs:ms-3 flex flex-col  sm:flex-row sm:justify-start items-start justify-center mt-2">
-            <button
+            <Button
+              primary={selectedCategory == "all" ? true : false}
               onClick={() => {
                 setselectedCategory("all");
                 setCurrentPage(1);
               }}
-              type="button"
-              className={`  ${
-                selectedCategory == "all"
-                  ? "bg-btn-primary  text-white hover:bg-btn-secondary  hover:text-slate-300  hover:outline-0 shadow-slate-700 shadow-md"
-                  : ""
-              }  hover:border-btn-primary text-white outline outline-btn-primary outline-1 hover:outline-white hover:bg-red-600 rounded-full  font-medium px-5 py-2.5 text-center mr-3 mb-3 `}
+
+              // className={`  ${
+              //   selectedCategory == "all"
+              //     ? " text-white hover:bg-btn-secondary  hover:text-slate-300  hover:outline-0 shadow-slate-700 shadow-md"
+              //     : ""
+              // }  hover:border-btn-primary text-white outline outline-btn-primary outline-1 hover:outline-white hover:bg-red-600 rounded-full  font-medium px-5 py-2.5 text-center mr-3 mb-3 `}
             >
               All categories
-            </button>
+            </Button>
 
             {categories.map((category, index) => (
-              <button
+              <Button
                 key={index}
                 type="button"
                 onClick={() => {
@@ -57,12 +47,12 @@ const SimpleGameFilter = ({
                 }  hover:border-btn-primary text-white outline outline-btn-primary outline-1 hover:outline-white hover:bg-red-600 rounded-full  font-medium px-5 py-2.5 text-center mr-3 mb-3 `}
               >
                 {category}
-              </button>
+              </Button>
             ))}
           </div>
           {/* price filter  */}
           <div className="border-t-2 sm:border-none flex flex-col  xs:ms-3 sm:flex-row sm:justify-start  items-start justify-center py-2">
-            <button
+            <Button
               onClick={() => {
                 handlePriceFilter(0);
               }}
@@ -74,9 +64,9 @@ const SimpleGameFilter = ({
               }  hover:border-btn-primary text-white outline outline-btn-primary outline-1 hover:outline-white hover:bg-red-600 rounded-full  font-medium px-5 py-2.5 text-center mr-3 mb-3 `}
             >
               All Prices
-            </button>
+            </Button>
 
-            <button
+            <Button
               type="button"
               onClick={() => {
                 handlePriceFilter(1);
@@ -88,9 +78,9 @@ const SimpleGameFilter = ({
               }  hover:border-btn-primary text-white outline outline-btn-primary outline-1 hover:outline-white hover:bg-red-600 rounded-full  font-medium px-5 py-2.5 text-center mr-3 mb-3 `}
             >
               1-10$
-            </button>
+            </Button>
 
-            <button
+            <Button
               type="button"
               onClick={() => {
                 handlePriceFilter(2);
@@ -102,9 +92,9 @@ const SimpleGameFilter = ({
               }  hover:border-btn-primary text-white outline outline-btn-primary outline-1 hover:outline-white hover:bg-red-600 rounded-full  font-medium px-5 py-2.5 text-center mr-3 mb-3 `}
             >
               11-20${" "}
-            </button>
+            </Button>
 
-            <button
+            <Button
               type="button"
               onClick={() => {
                 handlePriceFilter(3);
@@ -116,7 +106,7 @@ const SimpleGameFilter = ({
               }  hover:border-btn-primary text-white outline outline-btn-primary outline-1 hover:outline-white hover:bg-red-600 rounded-full  font-medium px-5 py-2.5 text-center mr-3 mb-3 `}
             >
               above 20$
-            </button>
+            </Button>
           </div>
         </div>
       )}
