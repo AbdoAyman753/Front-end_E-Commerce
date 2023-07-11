8a4bfb97dc301ee9ed7d5d47fa1f4fe9cb8522c7
Abdallah Mohamed <83477790+AbdallahM22@users.noreply.github.com>
2023-07-11 23:02:11 +0300

    Update Button.jsx

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -11,7 +11,7 @@ const Button = ({ children, primary = true, className = "", onClick }) => {
         primary
           ? "bg-btn-primary text-white hover:bg-btn-Secondary"
           : "border-2 border-btn-Secondary hover:bg-btn-primary"
-      } rounded-lg ms-2 px-2 py-1 text-md transition-all ${className}`}
+      } rounded-lg px-2 py-1 text-md transition-all ${className}`}
     >
       {children}
     </button>
