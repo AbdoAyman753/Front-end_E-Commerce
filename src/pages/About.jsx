49e3dc8d7ea6c6b32e9430f27a00734fff479736
HebaAli48 <131808003+HebaAli48@users.noreply.github.com>
2023-07-12 20:37:56 +0300

    added texts to about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,10 +2,10 @@ import { Carousel } from "antd";
 import pubg from "../assets/img/pubg.jpg";
 import ocean from "../assets/img/ocean.jpg";
 import fifa23 from "../assets/img/fifa23.jpg";
-import knight from "../assets/img/knight.png"
-import starship from "../assets/img/starship.webp"
-import glory from "../assets/img/glory.webp"
-import spyro from "../assets/img/spyro.jpg"
+import knight from "../assets/img/knight.png";
+import starship from "../assets/img/starship.webp";
+import glory from "../assets/img/glory.webp";
+import spyro from "../assets/img/spyro.jpg";
 const contentStyle = {
   color: "#fff",
   textAlign: "center",
@@ -14,7 +14,6 @@ const contentStyle = {
 
 const About = () => {
   return (
-
     ////////////////////////////////CAROUSEL///////////////////////////////////////
 
     <div className="about">
@@ -44,16 +43,14 @@ const About = () => {
         <div style={contentStyle}>
           <img className="w-full h-96" src={spyro}></img>
         </div>
-       
-       
       </Carousel>
 
       {/* //////////////////BOX//////////////////// */}
 
-      <div className="photo flex items-center justify-center mt-20 mb-20">
-        <div className="about_community rounded-lg	border-x-red-900		border-solid border w-9/12">
+      <div className="photo flex items-center justify-center mt-20 mb-20 ">
+        <div className="about_community rounded-lg w-4/5	border-solid border p-3 ">
           <div className="about_content pt-8">
-            <h2 className="about_title font-extrabold text-3xl text-red-700	 normal-case tracking-normal text-center mb-10 font-mono">
+            <h2 className="about_title font-extrabold text-4xl text-line-primary	 normal-case tracking-normal text-center mb-10 font-mono">
               Join Our Community
             </h2>
           </div>
@@ -76,8 +73,15 @@ const About = () => {
 
       {/* ///////////////THE VIDEO////////////// */}
 
-      <div className="about_video  overflow-hidden flex mt-2 justify-center items-center">
-        <div className="about_monitor_video  w-3/5 ">
+      <div className="about_video w-[80vw] overflow-hidden flex mt-2  justify-between  mx-auto  items-center">
+        <div className=" text-white ms-3">
+          <h1>#logo</h1>
+          <p className=" text-3xl ">
+            V9 Games is the ultimate destination for playing, discussing, and
+            creating games.
+          </p>
+        </div>
+        <div className="about_monitor_video  w-[70vw]">
           <video
             className="video w-full h-auto object-contain box-content overflow-clip "
             autoPlay
@@ -93,28 +97,33 @@ const About = () => {
 
       {/* //////////////////GAMES PHOTOS//////////////////// */}
 
-      <div className="about_imgs ">
-                <img
-                  className="about_game w-2/5 box-content overflow-clip float-right mr-10"
-                  src="https://cdn.cloudflare.steamstatic.com/store/about/cta_hero_hardware.png"
-                ></img>
-                <img
-                  className="about_game_img1 w-2/5 box-content overflow-clip "
-                  src="https://cdn.cloudflare.steamstatic.com/store/about/cta_hero_hardware_pt1.png"
-                ></img>
-                <img
-                  className="about_game_img2 w-2/5 box-content overflow-clip mb-5 ml-6"
-                  src="https://cdn.cloudflare.steamstatic.com/store/about/cta_hero_hardware_pt2.png"
-                ></img>
-              
-                <img
-                  className="about_game_img3 w-2/5 box-content overflow-clip float-right mr-6"
-                  src="https://cdn.cloudflare.steamstatic.com/store/about/cta_hero_steamworks.png "
-                ></img>
-                <img
-                  className="about_game_img3 w-2/5 box-content overflow-clip mb-8 "
-                  src="https://cdn.cloudflare.steamstatic.com/store/about/cta_hero_steamworks_pt1.png"
-                ></img>
+      <div className="about_imgs mt-5 ">
+        <img
+          className="about_game w-2/5 box-content overflow-clip float-right mr-10"
+          src="https://cdn.cloudflare.steamstatic.com/store/about/cta_hero_hardware.png"
+        ></img>
+        <img
+          className="about_game_img1 w-2/5 box-content overflow-clip "
+          src="https://cdn.cloudflare.steamstatic.com/store/about/cta_hero_hardware_pt1.png"
+        ></img>
+        <img
+          className="about_game_img2 w-2/5 box-content overflow-clip mb-5 ml-6"
+          src="https://cdn.cloudflare.steamstatic.com/store/about/cta_hero_hardware_pt2.png"
+        ></img>
+        {/* <div className="relative">
+          <img
+            className="about_game_img1 absolute top-0 left-0 w-2/5 box-content overflow-clip "
+            src="https://cdn.cloudflare.steamstatic.com/store/about/cta_hero_hardware_pt1.png"
+          ></img>
+          <img
+            className="about_game_img2 absolute top-0 left-0 w-2/5 box-content overflow-clip mb-5 ml-6"
+            src="https://cdn.cloudflare.steamstatic.com/store/about/cta_hero_hardware_pt2.png"
+          ></img>
+        </div> */}
+        <img
+          className="about_game_img3 w-2/5 box-content overflow-clip float-right mr-6"
+          src="https://cdn.cloudflare.steamstatic.com/store/about/cta_hero_steamworks.png "
+        ></img>
       </div>
     </div>
   );
