0848a14cba2bef8e30e252b9ffb7eeaf89f91ae6
AbdallahM22 <abdallahmbedo22@gmail.com>
2023-07-13 12:39:35 +0300

    added lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -2,87 +2,95 @@
 
 import "./App.css";
 import { Navigate, Route, Routes } from "react-router-dom";
+import { Suspense, lazy } from "react";
 
-import AppLayout from "./components/layout/AppLayout";
-import Support from "./pages/Support";
-import StorePage from "./pages/StorePage";
-import SignInPage from "./pages/SignInPage";
-import SignUpPage from "./pages/SignUpPage";
-import Cart from "./pages/Cart";
-import Wishlist from "./pages/Wishlist";
-import Profile from "./pages/Profile";
-import UserInfo from "./components/profile/UserInfo";
-import UserGames from "./components/profile/UserGames";
-import UserOrders from "./components/profile/UserOrders";
-import EditProfile from "./components/profile/editProfile";
-import EditPassword from "./components/profile/EditPassword";
-import GameProfile from "./pages/GameProfile";
-import Authentication from "./components/authentication/Authentication";
-import NotAuthentication from "./components/authentication/NotAuthentication";
-import PaymentSuccess from "./pages/PaymentSuccess";
-import About from "./pages/About";
-import Home from "./pages/Home";
+const Loader = lazy(() => import("./components/ui/Loader"));
+const AppLayout = lazy(() => import("./components/layout/AppLayout"));
+const Support = lazy(() => import("./pages/Support"));
+const StorePage = lazy(() => import("./pages/StorePage"));
+const SignInPage = lazy(() => import("./pages/SignInPage"));
+const SignUpPage = lazy(() => import("./pages/SignUpPage"));
+const Cart = lazy(() => import("./pages/Cart"));
+const Wishlist = lazy(() => import("./pages/Wishlist"));
+const Profile = lazy(() => import("./pages/Profile"));
+const UserInfo = lazy(() => import("./components/profile/UserInfo"));
+const UserGames = lazy(() => import("./components/profile/UserInfo"));
+const UserOrders = lazy(() => import("./components/profile/UserOrders"));
+const EditProfile = lazy(() => import("./components/profile/editProfile"));
+const EditPassword = lazy(() => import("./components/profile/EditPassword"));
+const GameProfile = lazy(() => import("./pages/GameProfile"));
+const Authentication = lazy(() =>
+  import("./components/authentication/Authentication")
+);
+const NotAuthentication = lazy(() =>
+  import("./components/authentication/NotAuthentication")
+);
+const PaymentSuccess = lazy(() => import("./pages/PaymentSuccess"));
+const About = lazy(() => import("./pages/About"));
+const Home = lazy(() => import("./pages/Home"));
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<AppLayout />}>
-        <Route index element={<Home />} />
-        <Route path="store" element={<StorePage />} />
-        <Route path="about" element={<About />} />
-        <Route
-          path="wishlist"
-          element={
-            <Authentication>
-              <Wishlist />
-            </Authentication>
-          }
-        />
-        <Route path="cart" element={<Cart />} />
-        <Route path="support" element={<Support />} />
-        <Route
-          path="user/"
-          element={
-            <Authentication>
-              <Profile />
-            </Authentication>
-          }
-        >
-          <Route index element={<Navigate replace to="info" />} />
-          <Route path="info" element={<UserInfo />} />
-          <Route path="games" element={<UserGames />} />
-          <Route path="orders" element={<UserOrders />} />
-          <Route path="edit" element={<EditProfile />} />
-          <Route path="edit-password" element={<EditPassword />} />
-          <Route path="*" element={<h2>not found</h2>} />
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="store" element={<StorePage />} />
+          <Route path="about" element={<About />} />
+          <Route
+            path="wishlist"
+            element={
+              <Authentication>
+                <Wishlist />
+              </Authentication>
+            }
+          />
+          <Route path="cart" element={<Cart />} />
+          <Route path="support" element={<Support />} />
+          <Route
+            path="user/"
+            element={
+              <Authentication>
+                <Profile />
+              </Authentication>
+            }
+          >
+            <Route index element={<Navigate replace to="info" />} />
+            <Route path="info" element={<UserInfo />} />
+            <Route path="games" element={<UserGames />} />
+            <Route path="orders" element={<UserOrders />} />
+            <Route path="edit" element={<EditProfile />} />
+            <Route path="edit-password" element={<EditPassword />} />
+            <Route path="*" element={<h2>not found</h2>} />
+          </Route>
+          <Route
+            path="sign-in"
+            element={
+              <NotAuthentication>
+                <SignInPage />
+              </NotAuthentication>
+            }
+          />
+          <Route
+            path="sign-up"
+            element={
+              <NotAuthentication>
+                <SignUpPage />
+              </NotAuthentication>
+            }
+          />
+          <Route path="game/:id" element={<GameProfile />} />
+          <Route
+            path="payment-success"
+            element={
+              <Authentication>
+                <PaymentSuccess />
+              </Authentication>
+            }
+          />
         </Route>
-        <Route
-          path="sign-in"
-          element={
-            <NotAuthentication>
-              <SignInPage />
-            </NotAuthentication>
-          }
-        />
-        <Route
-          path="sign-up"
-          element={
-            <NotAuthentication>
-              <SignUpPage />
-            </NotAuthentication>
-          }
-        />
-        <Route path="game/:id" element={<GameProfile />} />
-        <Route
-          path="payment-success"
-          element={
-            <Authentication>
-              <PaymentSuccess />
-            </Authentication>
-          }
-        />
-      </Route>
-    </Routes>
+      </Routes>
+    </Suspense>
   );
 }
 
