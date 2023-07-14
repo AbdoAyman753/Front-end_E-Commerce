7a81bf2d40d7e732571a299558638477ad3a9200
AbdallahM22 <abdallahmbedo22@gmail.com>
2023-07-14 21:48:33 +0300

    remove lazy loading from loader image

--- src/components/ui/Loader.jsx
+++ src/components/ui/Loader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loader = () => {
   return (
     <div className="text-center">
-      <img src="loading2.png" alt="Loading" className="m-auto" loading="lazy"/>
+      <img src="loading2.png" alt="Loading" className="m-auto" />
     </div>
   );
 };
