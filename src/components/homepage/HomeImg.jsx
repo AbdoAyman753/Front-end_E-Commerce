dcb6239d38e99f3f5d058ba6628351c0b0098e83
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-07-14 18:45:59 +0300

    added welcome message to home cover page

--- src/components/homepage/HomeImg.jsx
+++ src/components/homepage/HomeImg.jsx
@@ -1,22 +1,27 @@
-import home from '../../assets/img/home.gif'
+import home from '../../assets/img/home.gif';
+import Logo from '../ui/Logo';
+import { Link } from 'react-router-dom';
 const HomeImg = () => {
-    return (
-        
-        <div
-        className="img_home h-[86vh] bg-no-repeat w-full bg-cover"
-        style={{ backgroundImage: `url(${home})` }}
-      >
-        {/* <div className="text font-extrabold text-6xl w-1/2	text-white font-mono z-10 flex mt-32">
-          <div>
-            <p>
-              Welcome to{" "}
-              <span className="text-yellow-300 text-7xl ">v9Games</span>{" "}
-            </p>
-            
-          </div>
-        </div> */}
-    </div>
-     );
-}
- 
+	return (
+		<div
+			className='img_home h-[86vh] bg-no-repeat w-full bg-cover'
+			style={{ backgroundImage: `url(${home})` }}
+		>
+			<div className='font-extrabold text-8xl text-white font-mono z-10 flex justify-center mt-20'>
+				<div className='flex-col justify-center items-center'>
+					<p>Welcome</p>
+					<Link to='/store'>
+						<Logo
+							className={
+								'text-center border-white border rounded-lg bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg inset-0 z-10 text-7xl hover:scale-[102%] hover:bg-slate-800'
+							}
+							numClass={'text-7xl'}
+						/>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+};
+
 export default HomeImg;
